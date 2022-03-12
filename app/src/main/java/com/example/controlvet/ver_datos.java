de509e059067f128f8b97c323e96986441d4598e
Jesus-Damian <jesusdh01gmail.com>
2022-03-11 22:29:13 -0600

    R03
    Implementación del metodo eliminar en la clase de ver datos

--- app/src/main/java/com/example/controlvet/ver_datos.java
+++ app/src/main/java/com/example/controlvet/ver_datos.java
@@ -1,13 +1,17 @@
 package com.example.controlvet;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.example.controlvet.bd.DbMascotas;
 
 public class ver_datos extends AppCompatActivity {
 
@@ -23,7 +27,19 @@ public class ver_datos extends AppCompatActivity {
         btnEliminar1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                AlertDialog.Builder builder = new AlertDialog.Builder(ver_datos.this);
+                builder.setMessage("¿Desea eliminar este registro?").setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).show();
             }
         });
 
