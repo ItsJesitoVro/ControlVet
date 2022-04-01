5328c08c66630356175ff32dae072802066d2877
Jezer Alfredo Pérez Cruz <itsjesito@gmail.com>
2022-03-31 19:35:15 -0600

    Login v2
    Se hicieron correcciones al Login

--- app/src/main/java/com/example/controlvet/Login.java
+++ app/src/main/java/com/example/controlvet/Login.java
@@ -2,6 +2,7 @@ package com.example.controlvet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -17,25 +18,30 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        TextView username =(TextView) findViewById(R.id.username);
+        TextView usuario =(TextView) findViewById(R.id.txtusuario);
         TextView password =(TextView) findViewById(R.id.password);
 
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.btnlogin);
 
         //admin and admin
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                if(usuario.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     //correct
-                    Toast.makeText(Login.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Login.this,"SESIÓN INICIADA CORRECTAMENTE",Toast.LENGTH_SHORT).show();
+                    principal();
                 }else
                     //incorrect
-                    Toast.makeText(Login.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Login.this,"USUARIO O CONTRASEÑA ERRONEAS",Toast.LENGTH_SHORT).show();
             }
         });
 
 
+    }
+    public void principal(){
+        Intent principal = new Intent(this, MainActivity.class);
+        startActivity(principal);
     }
 }
